5ed9ee34673437ac63e721ff0044f16c2d1f26ba
zunggzing <hello.trungvinh@gmail.com>
2021-09-16 14:13:05 +0700

    Fix editor name in user

--- Zalo_Client/src/public/template/js/home.js
+++ Zalo_Client/src/public/template/js/home.js
@@ -7,6 +7,7 @@ const tabs = $$('.tab-item');
 const panes = $$('.tab-pane');
 const tabActive = $('.tab-item.active');
 
+
 tabs.forEach((tab, index) => {
     const pane = panes[index];
 
@@ -52,3 +53,27 @@ cardHeaderClickAtt.onclick = function () {
         cardBodyClickAtt.style.display = 'none';
     }
 };
+// console.log(editorParent.style)
+const editorName = document.querySelector('.txt-name-default');
+const editorParent = document.querySelector('#editor-name-parent');
+editorName.onclick = function () {
+    var input = editorName.innerHTML
+    // console.log(input)
+    var preEntry = input.substring(0, input.indexOf("<span> ") + 7)
+    // console.log(preEntry)
+    var entry = input.substring(input.indexOf("<span> ") + 6, input.indexOf("</span>"))
+    // console.log(entry)
+    var
+        postEntry = input.substring(input.indexOf("</span>"));
+
+    editorParent.innerHTML = ('<input name="txt-name" type="text" value="' +
+        entry + '">' +
+        postEntry);
+
+    Object.assign(editorParent.style, {
+        textAlign: "center",
+        marginTop: "10px",
+        fontSize: "20px",
+        fontWeight: "bold"
+    })
+}
