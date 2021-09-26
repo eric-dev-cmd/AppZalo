0402c6c4f21ff3af72f995e8b23e56c03eeb1388
zunggzing <hello.trungvinh@gmail.com>
2021-09-27 00:52:35 +0700

    Fix modal

--- Zalo_Client/src/public/js/contact.js
+++ Zalo_Client/src/public/js/contact.js
@@ -115,31 +115,48 @@ function removeRequestContact(receiverId) {
 
 //lắng nghe socket response-add-new-contact từ server
 socket.on('response-add-new-contact', function (user) {
-  let notification = `<li class="position-relative" data-uid = '${userId}'>
-            <a  style="width: 100%;">
-                <div class="d-flex">
-                    <div
-                        class="chat-user-img away align-self-center me-3 ms-0">
-                        <img src="images/${userAvatar}"
-                            class="rounded-circle avatar-xs" alt="">
-                        <span class="user-status"></span>
-                    </div>
-                    <div class="flex-1 overflow-hidden">
-                        <h5 class="text-truncate font-size-15 mb-1">
-                        ${userName}</h5>
-                        <p class="chat-user-message text-truncate mb-0">
-                            Muốn kết bạn. "Xin chào, tôi là <span> ${userName}.
-                        </p>
-        
-        
-                    </div>
+  let notification = `<li class="position-relative" data-uid = '${user.id}'>
+        <a  style="width: 90%;">
+            <div class="d-flex">
+                <div
+                    class="chat-user-img away align-self-center me-3 ms-0">
+                    <img src="images/${user.avatar}"
+                        class="rounded-circle avatar-xs" alt="">
+                    <span class="user-status"></span>
                 </div>
-            </a>
-          <div class="d-flex justify-content-around">
-            <a class="cursor-point"  id="btn-cancel-friend-receiver"  data-uid = '${userId}' >Bỏ qua</a>
-            <a class="cursor-point" id="btn-accept-friend" data-uid = '${userId}' >Đồng ý</a>
-          </div>
-        </li>`;
+                <div class="flex-1 overflow-hidden">
+                    <h5 class="text-truncate font-size-15 mb-1">
+                    ${user.userName}</h5>
+                    <p class="chat-user-message text-truncate mb-0">
+                        Muốn kết bạn. "Xin chào, tôi là <span>Bảo Anh.
+                    </p>
+    
+    
+                </div>
+            </div>
+        </a>
+        <div style="float: right;position: absolute;
+    top: 14px;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex: 1;
+    width: 14%;">
+            <div class="fs-13 pb-1">
+                <a  id="btn-cancel-friend-receiver" data-uid = '${user.id}'
+                    class="text-decoration-none cursor-point" style="    position: static;
+    padding: 0;
+    display: inline-block;" >Bỏ qua</a>
+            </div>
+            <div class="fs-13">
+                <a  id="btn-accept-friend" data-uid = '${user.id}'
+                    class="text-decoration-none cursor-point" style="    position: static;
+    padding: 0;
+    display: inline-block;">Đồng ý</a>
+            </div>
+        </div>
+    </li>`;
   $('#notification-contact').prepend(notification);
   removeRequestContactReceiver();
   acceptRequestContact();
