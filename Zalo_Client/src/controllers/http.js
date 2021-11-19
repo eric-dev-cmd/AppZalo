2059bdb4d94df655802043de1b4d8819191db0cf
catluynh <catluynh99@gmail.com>
2021-11-19 16:50:22 +0700

    update http

--- Zalo_Client/src/controllers/http.js
+++ Zalo_Client/src/controllers/http.js
@@ -1,3 +1,4 @@
-const http = 'http://ec2-52-221-232-72.ap-southeast-1.compute.amazonaws.com:4000';
+const http = 'http://ec2-18-142-51-89.ap-southeast-1.compute.amazonaws.com:4000';
+
 
 module.exports = http;
