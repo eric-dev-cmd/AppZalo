b99bdb6e9fed0a826e9f1ac95b27f672d45b19ee
catluynh <catluynh99@gmail.com>
2021-11-20 11:00:36 +0700

    http

--- Zalo_Client/src/controllers/http.js
+++ Zalo_Client/src/controllers/http.js
@@ -1,4 +1,4 @@
-const http = 'http://ec2-54-251-137-82.ap-southeast-1.compute.amazonaws.com:4000';
+const http = 'http://ec2-3-0-54-190.ap-southeast-1.compute.amazonaws.com:4000';
 
 
 module.exports = http;
--- Zalo_Client/src/public/js/mainConfig.js
+++ Zalo_Client/src/public/js/mainConfig.js
@@ -1,5 +1,5 @@
 const socket = io();
-const http = `http://ec2-54-251-137-82.ap-southeast-1.compute.amazonaws.com:4000`;
+const http = `http://ec2-3-0-54-190.ap-southeast-1.compute.amazonaws.com:4000`;
 //các cuộc trò truyện
 let conversations;
 const s3 = 'https://stores3appchatmobile152130-dev.s3.ap-southeast-1.amazonaws.com/public'
