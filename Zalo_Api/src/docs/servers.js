886db206570451417c9752a74f4f0f22f2fb185e
zunggzing <hello.trungvinh@gmail.com>
2021-11-21 20:22:53 +0700

    Update http

--- Zalo_Api/src/docs/servers.js
+++ Zalo_Api/src/docs/servers.js
@@ -1,7 +1,7 @@
 module.exports = {
   servers: [
     {
-      url: "http://ec2-13-213-71-132.ap-southeast-1.compute.amazonaws.com:4000",
+      url: "http://localhost:4000",
       description: "Swagger API",
     },
   ],
--- Zalo_Client/src/controllers/http.js
+++ Zalo_Client/src/controllers/http.js
@@ -1,5 +1,4 @@
-const http =
-  'http://ec2-13-213-71-132.ap-southeast-1.compute.amazonaws.com:4000';
+const http = 'http://localhost:4000';
 
 
 module.exports = http;
