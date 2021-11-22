533f324bb6a37018bd94dc0ef0406b530cf63a6c
zunggzing <hello.trungvinh@gmail.com>
2021-11-22 12:42:02 +0700

    Add pin

--- Zalo_Api/src/models/chatGroupModel.js
+++ Zalo_Api/src/models/chatGroupModel.js
@@ -1,23 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ChatGroup = new Schema({
-    name: String,
-    userAmount: { type: Number, min: 2, max: 200 },
-    userId: String,
-    members: [
-        { userId: String }
-    ],
-    avatar: {
-        type: String,
-        default: "group-avatar.png",
-    },
-    background: {
-        type: String,
-        default: "background-default.jpg",
-    },
-    createdAt: { type: Number, default: Date.now },
-    updatedAt: { type: Number, default: Date.now }
+  name: String,
+  userAmount: { type: Number, min: 2, max: 200 },
+  userId: String,
+  members: [{ userId: String }],
+  avatar: {
+    type: String,
+    default: "group-avatar.png",
+  },
+  background: {
+    type: String,
+    default: "background-default.jpg",
+  },
+  pin: [{ messageId: String }, { userId: String }],
+  createdAt: { type: Number, default: Date.now },
+  updatedAt: { type: Number, default: Date.now },
 });
 
-module.exports = mongoose.model('chat-group', ChatGroup);
+module.exports = mongoose.model("chat-group", ChatGroup);
